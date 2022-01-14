64e25aa8de452ab8d022465d4046b15d6eec009e
ERKAM GURESEN <erkamguresen@gmail.com>
2022-01-14 21:04:47 +0100

    city and phto classes

--- city-guide/src/app/city/city.component.ts
+++ city-guide/src/app/city/city.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { City } from '../city';
 
 @Component({
   selector: 'app-city',
   templateUrl: './city.component.html',
-  styleUrls: ['./city.component.css']
+  styleUrls: ['./city.component.css'],
 })
 export class CityComponent implements OnInit {
+  city: City[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
