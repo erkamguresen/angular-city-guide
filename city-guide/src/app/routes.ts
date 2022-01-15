d7b38f07c28e7b2060350b7493822d1083aa8ebe
ERKAM GURESEN <erkamguresen@gmail.com>
2022-01-15 09:48:06 +0100

    new route

--- city-guide/src/app/routes.ts
+++ city-guide/src/app/routes.ts
@@ -4,6 +4,7 @@ import { NavComponent } from './nav/nav.component';
 
 export const APP_ROUTES: Routes = [
   { path: '', component: CityComponent },
+  { path: 'cities', component: CityComponent },
   { path: 'nav', component: NavComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
